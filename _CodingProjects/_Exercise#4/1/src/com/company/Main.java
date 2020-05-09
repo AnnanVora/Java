149cb1f3fdd4767d49ed8dde3571ef67bc29cba9
AnnanVora <annan.vora@gmail.com>
2020-05-09 12:41:45 +0500

    Exercise4 1 full commit

--- _CodingProjects/_Exercise#4/1/src/com/company/Main.java
+++ _CodingProjects/_Exercise#4/1/src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] values = new int[] {1, 2, 3, 2, 1};
+        isPalindrome(values);
+    }
+
+    public static void isPalindrome(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (!(array[i] == (array[(array.length - 1) - i]))) {
+                System.out.println("The array is not a palindrome");
+                return;
+            }
+        }
+        System.out.println("The array is a palindrome");
     }
 }
